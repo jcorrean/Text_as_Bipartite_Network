914bc7e1fee0a4e3b2da581c913acc4c76678c48
Juan <juan@pop-os.localdomain>
2021-02-19 11:18:44 -0500

    Tutorial Script

--- Examples.R
+++ Examples.R
@@ -58,23 +58,3 @@ plot(g_rest,
      vertex.label.cex = 0.8, 
      vertex.label.color = "black", 
      layout = layout_with_dh)
-
-# Example 2 
-load("/home/juan/SkillsOccupations.RData")
-library(dplyr)
-Scientists <- filter(skills, grepl("Scientist", Occupation))
-
-library(igraph)
-bn2 <- graph.data.frame(Scientists,directed=FALSE)
-bipartite.mapping(bn2)
-
-V(bn2)$type <- bipartite_mapping(bn2)$type
-V(bn2)$color <- ifelse(V(bn2)$type, "lightblue", "yellow")
-V(bn2)$shape <- ifelse(V(bn2)$type, "circle", "square")
-V(bn2)$label.cex <- ifelse(V(bn2)$type, 0.8, 1.5)
-V(bn2)$size <- (Scientists$Importance)/2
-E(bn2)$color <- "black"
-plot(bn2,
-     vertex.label.color = "black",
-     layout = layout_with_graphopt, main = "")
-
