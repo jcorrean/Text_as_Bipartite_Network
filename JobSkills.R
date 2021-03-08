4ebc6e1217efb6ecbfa4df061ba8a7fe2cafed36
Juan C. Correa <juanc.correan@konradlorenz.edu.co>
2021-03-08 18:50:49 -0500

    Short Script Job-Skills Example

---
+++ JobSkills.R
@@ -0,0 +1,15 @@
+# Example for Job Titles and Skills
+library(dplyr)
+Scientists <- filter(skills, grepl("Scientist", Occupation))
+library(igraph)
+bn2 <- graph.data.frame(Scientists,directed=FALSE)
+bipartite.mapping(bn2)
+V(bn2)$type <- bipartite_mapping(bn2)$type
+V(bn2)$color <- ifelse(V(bn2)$type, "lightblue", "yellow")
+V(bn2)$shape <- ifelse(V(bn2)$type, "circle", "square")
+V(bn2)$label.cex <- ifelse(V(bn2)$type, 0.8, 1)
+V(bn2)$size <- (Scientists$Importance)/3
+E(bn2)$color <- "black"
+plot(bn2,
+     vertex.label.color = "black",
+     layout = layout_with_graphopt, main = "")
